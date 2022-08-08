b176e6bdf8350de1af83362b01012a76925c142c
Charles-Henri <chdecultot@dokos.io>
2022-08-08 13:41:02 +0200

    fix: Update old JSON files format

---
+++ hrms/hr/doctype/daily_work_summary_group/test_daily_work_summary_group.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and Contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+class TestDailyWorkSummaryGroup(FrappeTestCase):
+	pass
