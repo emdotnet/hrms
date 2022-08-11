936731212a0a6693f75d97900f48f690b2b05a7b
Charles-Henri <chdecultot@dokos.io>
2022-08-11 14:53:59 +0200

    fix: leave types cration in tests

--- hrms/hr/doctype/leave_period/test_leave_period.py
+++ hrms/hr/doctype/leave_period/test_leave_period.py
@@ -22,6 +22,10 @@ def create_leave_period(from_date, to_date, company=None):
 			from_date=from_date,
 			to_date=to_date,
 			is_active=1,
+			leave_types=[{
+				"leave_type": lt.name
+			} for lt in frappe.get_all("Leave Type")]
+
 		),
 		"name",
 	)
@@ -35,6 +39,10 @@ def create_leave_period(from_date, to_date, company=None):
 			"from_date": from_date,
 			"to_date": to_date,
 			"is_active": 1,
+			"leave_types": [{
+				"leave_type": lt.name
+			} for lt in frappe.get_all("Leave Type")]
+
 		}
 	).insert()
 	return leave_period
