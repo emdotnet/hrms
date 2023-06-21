e79abd89d0995f832a2531b75f462ea2e527ca67
Charles-Henri Decultot <chdecultot@dokos.io>
2023-06-21 09:40:17 +0200

    refactor: set fetch_from for company in Attendance dt and remove unnecessary db calls

--- hrms/hr/doctype/attendance/attendance.js
+++ hrms/hr/doctype/attendance/attendance.js
@@ -1,27 +1,16 @@
-// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+// Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and Contributors
 // License: GNU General Public License v3. See license.txt
 
-frappe.ui.form.on("Attendance Request", {
+frappe.ui.form.on("Attendance", {
 	refresh(frm) {
-		frm.trigger("show_attendance_warnings");
-	},
-
-	show_attendance_warnings(frm) {
-		if (!frm.is_new() && frm.doc.docstatus === 0) {
-			frm.dashboard.clear_headline();
-
-			frm.call("get_attendance_warnings").then((r) => {
-				if (r.message?.length) {
-					frm.dashboard.reset();
-					frm.dashboard.add_section(
-						frappe.render_template("attendance_warnings", {
-							warnings: r.message || [],
-						}),
-						__("Attendance Warnings")
-					);
-					frm.dashboard.show();
-				}
-			})
+		if (frm.doc.__islocal && !frm.doc.attendance_date) {
+			frm.set_value("attendance_date", frappe.datetime.get_today());
 		}
-	}
-});
+
+		frm.set_query("employee", () => {
+			return {
+				query: "erpnext.controllers.queries.employee_query"
+			}
+		})
+	},
+})
