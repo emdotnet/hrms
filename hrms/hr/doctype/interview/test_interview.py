04795d21da0c3f63dff8b2052d373a12a8f81b19
dokos-bot 🤖 <15133794-dokos-bot@users.noreply.gitlab.com>
2023-07-27 18:07:53 +0000

    Merge pull request #734 from ruchamahabal/use-FrappeTestCase

--- hrms/hr/doctype/interview/test_interview.py
+++ hrms/hr/doctype/interview/test_interview.py
@@ -7,7 +7,7 @@ import os
 import frappe
 from frappe import _
 from frappe.core.doctype.user_permission.test_user_permission import create_user
-from frappe.tests.utils import FrappeTestCase, change_settings
+from frappe.tests.utils import FrappeTestCase
 from frappe.utils import add_days, get_time, getdate, nowtime
 
 from erpnext.setup.doctype.designation.test_designation import create_designation
